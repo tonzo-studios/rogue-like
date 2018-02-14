672ec9dd64fea2c59ced651cc7e3628fdc1fee06
Daniel Monzonís <monzo94@users.noreply.github.com>
2018-02-14 13:17:02 +0100

    Add vectors (#5)
    A vector class adds a layer of abstraction for operations with positions, distances, etc, by using 2D Euclidean vectors.
    
    It provides a nice way to perform element-wise operations with vectors by overriding the addition and subtraction operators. It also has a norm property which will return the norm of the vector, and a method to return a normalized vector with the same direction.

---
+++ vector.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from math import sqrt
+
+
+class Vector:
+    """Represents a vector in a 2D Euclidean space.
+
+    Can be used to represent position, distance, direction, etc.
+    Overrides the addition and subtraction operators to perform element-wise operations.
+    """
+
+    def __init__(self, x, y):
+        """Initialize the vector's components."""
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        """Perform element-wise addition."""
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        """Perform element-wise subtraction."""
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __repr__(self):
+        """Represent the vector as a tuple."""
+        return f"({self.x}, {self.y})"
+
+    @property
+    def norm(self):
+        """Return the norm of the vector."""
+        return sqrt(self.x ** 2 + self.y ** 2)
+
+    def normalized(self):
+        """Return a unitary vector with the same direction."""
+        norm = self.norm()
+        return Vector(self.x / norm, self.y / norm)
