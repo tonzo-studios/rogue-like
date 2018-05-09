0f91823322a6b4c3ad73847ccf2338639bb2486b
monzo94 <monzo_94@hotmail.com>
2018-05-09 13:18:46 +0200

    Add more entity tests

--- tests/test_entities.py
+++ tests/test_entities.py
@@ -38,18 +38,18 @@ def candy():
 
 
 @pytest.fixture
-def game_map(player, orc, air, candy):
-    """Create a level and place the other objects in it."""
+def game_map(player, air, candy):
+    """Create a level and place other objects in it."""
     from misc import Vector
     from dungeon import Dungeon
     from registry import Registry
     registry = Registry()
     dungeon = Dungeon()
+    dungeon.clear()
     dungeon.initialize(player, registry)
     level = dungeon.current_level
     # Place player and orc in this level
     player.place(level, Vector(0, 0))
-    orc.place(level, Vector(5, 5))
     candy.place(level, Vector(1, 0))
     air.place(level, Vector(0, 1))
     return level
@@ -83,6 +83,38 @@ class TestEntity(object):
         player.exp += 100
         assert player.level == 2
         assert player.exp == 17
+
+    def test_entity_placement(self, game_map, orc):
+        from misc import Vector
+        assert orc.game_map is None
+        assert orc.pos == Vector(0, 0)
+        orc.place(game_map, Vector(5, -2))
+        assert orc.game_map == game_map
+        assert orc.pos == Vector(5, -2)
+        assert orc in game_map.entities
+
+    def test_blocking_entity_in_map(self, game_map, orc):
+        from misc import Vector
+        # Force set walkable on origin
+        game_map.walkable[Vector(0, 0)] = True
+        orc.place(game_map, Vector(0, 0))
+        assert not game_map.walkable[Vector(0, 0)]
+        orc.place(game_map, Vector(1, 1))
+        assert game_map.walkable[Vector(0, 0)]
+
+    def test_unblocking_entity_in_map(self, game_map, air):
+        from misc import Vector
+        # Force set walkable on origin
+        game_map.walkable[Vector(0, 0)] = True
+        air.place(game_map, Vector(0, 0))
+        assert game_map.walkable[Vector(0, 0)]
+        air.place(game_map, Vector(1, 1))
+        assert game_map.walkable[Vector(0, 0)]
+
+    def test_item_interaction_exhaust(self, game_map, player, candy):
+        assert candy in game_map.entities
+        candy.use(player)
+        assert candy not in game_map.entities
 
 
 class TestBackpackItem(object):
